9230895373709de6cdc25d4f9d870c67da73ac72
Kamila Alencar <kamilaalencar1998@gmail.com.(none)>
2021-11-24 18:13:31 -0300

    organizando

--- ass-jokenpo/jokenpo.js
+++ ass-jokenpo/jokenpo.js
@@ -1,11 +1,9 @@
-let jogador = 
+let jogador = ""
 
 const botoes = document.querySelectorAll('button')
 console.log(botoes)
 botoes.forEach((button) => button.addEventListener('click', function (evt) { 
-   jogador = evt.target.id
-  
-
+   jogadorEscolha = evt.target.id
     
 }))
 
